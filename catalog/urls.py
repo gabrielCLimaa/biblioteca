498f082857c6c068135a508a62d42d2719c885f7
gabrielCLimaa <gabrielcrcl12345@gmail.com>
2022-01-15 12:52:27 -0300

    Index page added

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def index(request):
+
+    num_books = Book.objects.count()
+    num_books_instance = BookInstace.objects.count()
+
+    num_available_books = BookInstace.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books' : num_books,
+        'num_authors' : num_authors,
+        'num_books_instance' : num_books_instance,
+        'num_available_books' : num_available_books
+    }
+
+    return render(request, 'index.html', context=context)
