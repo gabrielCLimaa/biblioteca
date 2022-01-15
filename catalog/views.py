64db24f849b2277866222b6c67fb427f2998e785
gabrielCLimaa <gabrielcrcl12345@gmail.com>
2022-01-15 15:23:43 -0300

    Visitors controll added

--- catalog/views.py
+++ catalog/views.py
@@ -5,19 +5,20 @@ from .models import *
 # Create your views here.
 
 def index(request):
-
     num_books = Book.objects.count()
     num_books_instance = BookInstance.objects.count()
-
     num_available_books = BookInstance.objects.filter(status__exact='a').count()
-
     num_authors = Author.objects.count()
+
+    num_visitors = request.session.get('num_visitors', 0)
+    request.session['num_visitors'] = num_visitors + 1
 
     context = {
         'num_books' : num_books,
         'num_authors' : num_authors,
         'num_books_instance' : num_books_instance,
-        'num_available_books' : num_available_books
+        'num_available_books' : num_available_books,
+        'num_visitors' : num_visitors
     }
 
     return render(request, 'index.html', context=context)
